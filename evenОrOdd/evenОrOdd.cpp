ecf4ed142758214746862fbeb943fb7ee84463ee
MariaIvanova01 <mimi200801@abv.bg>
2021-03-25 18:52:27 +0200

    Logic implemented

---
+++ evenОrOdd/evenОrOdd.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	cout << " a = ";
+	cin >> a;
+
+	if (a % 2 == 0)
+	{
+		cout << " a is even " << endl;
+
+	}
+	else if (a % 2 != 0)
+	{
+		cout << " a is odd " << endl;
+	}
+	
+	
+	system("PAUSE");
+	return 0;
+}
